1fcca12005a08d80cd34ea9fe8a12ecd5286f4ed
Constantine Molchanov <moigagoo@duck.com>
2024-07-21 22:43:19 +0400

    Switch from Table refs to Tables.

--- src/kraut/types.nim
+++ src/kraut/types.nim
@@ -10,6 +10,6 @@ type
   Renderer* = proc(context: Context): VNode {.nimcall.}
   Route* = tuple[pattern: string, renderer: Renderer]
   Context* = object of RootObj
-    urlParams*: TableRef[string, string]
-    qryParams*: TableRef[string, string]
+    urlParams*: Table[string, string]
+    qryParams*: Table[string, string]
 
